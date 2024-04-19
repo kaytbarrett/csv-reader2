69b55b9639279c8d26e311b87214ec98e1186a85
Katie Barrett <kayt.barrett22@gmail.com>
2024-04-18 21:15:28 -0600

    initial commit

---
+++ CsvReader2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    ifstream inputFile("data.csv");
+    ofstream outputFile("noCommaData.csv");
+    if(!inputFile.is_open() || !outputFile.is_open()){
+        cout << "Error: file is unable to open" << endl;
+    }
+
+    string line;
+    while(getline(inputFile, line)){
+        size_t pos;
+        while((pos = line.find(',')) != string::npos){
+            line.erase(pos, 1);
+            line.insert(pos, " ");
+            pos++; 
+        }
+        outputFile << line << '\n' << endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+
+    return 0;
+}
